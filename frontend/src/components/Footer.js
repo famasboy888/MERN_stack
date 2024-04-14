b48d5caa3ca6162f0748229834e369ef8910878a
famasboy888 <famasboy88@gmail.com>
2024-04-14 23:31:36 +0800

    Updated Home workout list

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer>
+            <div className="container">
+                MERN Stack | Kyle Yap @ 2024
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return ( 
+    return (
         <header>
+            <div className="pull-right">
+                <img width={230} height={90} src="/mern.png" alt="mern" />
+            </div>
             <div className="container">
                 <Link to="/">
-                    <h1>Workout Buddy</h1>
+                    <h1>Workout List</h1>
                 </Link>
             </div>
         </header>
-     );
+    );
 }
- 
+
 export default Navbar;
